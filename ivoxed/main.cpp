a86f84ba73db1cbc97c0aea919ecf3211a35198a
netpipe <netpipe@localhost.localdomain>
2024-07-15 14:23:50 -0600

    testing

--- ivoxed/main.cpp
+++ ivoxed/main.cpp
@@ -1,225 +1,235 @@
+#include <irrlicht/irrlicht.h>
 #include <QApplication>
-#include <QMainWindow>
-#include <QOpenGLWidget>
-#include <QOpenGLFunctions>
-#include <QMouseEvent>
+#include <QWidget>
+#include <QVBoxLayout>
+#include <QPushButton>
 #include <QFileDialog>
-#include <QMenuBar>
-#include <vector>
-#include <fstream>
+#include <QTimer>
+#include <QMouseEvent>
 #include <iostream>
-#include <GL/glut.h>
+#include <unordered_map>
+#include "VoxelNode.h"
 
+using namespace irr;
 
-class OpenGLWidget : public QOpenGLWidget, protected QOpenGLFunctions {
+class VoxelEditor : public QWidget, public IEventReceiver {
     Q_OBJECT
 
 public:
-    OpenGLWidget(QWidget *parent = nullptr)
-        : QOpenGLWidget(parent), gridSize(10), voxelSize(1.0f), snapToGrid(true), zoomLevel(15.0f), cameraX(0.0f), cameraY(0.0f) {
-        voxelGrid.resize(gridSize * gridSize * gridSize, false);
-    }
+    VoxelEditor();
+    ~VoxelEditor();
+    void run();
 
-public slots:
-    // void loadVoxels() {
-    //     QString fileName = QFileDialog::getOpenFileName(this, "Open Voxel File", "", "Voxel Files (*.vox)");
-    //     if (!fileName.isEmpty()) {
-    //         std::ifstream file(fileName.toStdString(), std::ios::binary);
-    //         if (file.is_open()) {
-    //             file.read(reinterpret_cast<char*>(voxelGrid.data()), voxelGrid.size() * sizeof(bool));
-    //             file.close();
-    //             update();
-    //         }
-    //     }
-    // }
-
-    // void saveVoxels() {
-    //     QString fileName = QFileDialog::getSaveFileName(this, "Save Voxel File", "", "Voxel Files (*.vox)");
-    //     if (!fileName.isEmpty()) {
-    //         std::ofstream file(fileName.toStdString(), std::ios::binary);
-    //         if (file.is_open()) {
-    //             file.write(reinterpret_cast<const char*>(voxelGrid.data()), voxelGrid.size() * sizeof(bool));
-    //             file.close();
-    //         }
-    //     }
-    // }
+    // Override the OnEvent function to handle events
+    virtual bool OnEvent(const SEvent &event);
 
 protected:
-    void initializeGL() override {
-        initializeOpenGLFunctions();
-        glEnable(GL_DEPTH_TEST);
-        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+    void mousePressEvent(QMouseEvent* event) override;
+    void mouseReleaseEvent(QMouseEvent* event) override;
+    void mouseMoveEvent(QMouseEvent* event) override;
+
+private slots:
+    void onSelectTexture();
+    void onUpdate();
+
+private:
+    void createScene();
+    void placeVoxel(const core::vector3df &position);
+    void removeVoxel(scene::ISceneNode *node);
+    void scaleVoxel(VoxelNode* voxelNode, float scale);
+
+    IrrlichtDevice *mDevice;
+    video::IVideoDriver *mDriver;
+    scene::ISceneManager *mSceneMgr;
+    scene::ICameraSceneNode *mCamera;
+    bool mRunning;
+    std::string mCurrentTexture;
+    scene::ISceneCollisionManager* cm;
+
+    QTimer *mTimer;
+    u32 mLastClickTime;
+    core::vector3df mLastClickPos;
+    std::unordered_map<scene::ISceneNode *, VoxelNode *> mVoxelMap;
+
+    bool mLeftMousePressed;
+    bool mRightMousePressed;
+};
+
+VoxelEditor::VoxelEditor()
+    : mDevice(nullptr),
+      mDriver(nullptr),
+      mSceneMgr(nullptr),
+      mCamera(nullptr),
+      mRunning(true),
+      mLastClickTime(0),
+      mLeftMousePressed(false),
+      mRightMousePressed(false) {
+    mCurrentTexture = "default.png";
+
+    QVBoxLayout *layout = new QVBoxLayout(this);
+    QPushButton *textureButton = new QPushButton("Select Texture", this);
+    layout->addWidget(textureButton);
+
+    connect(textureButton, &QPushButton::clicked, this, &VoxelEditor::onSelectTexture);
+
+    mTimer = new QTimer(this);
+    connect(mTimer, &QTimer::timeout, this, &VoxelEditor::onUpdate);
+    mTimer->start(16);  // roughly 60 FPS
+
+    mDevice = createDevice(video::EDT_OPENGL, core::dimension2d<u32>(800, 600), 16, false, false, false, this);
+    if (!mDevice) {
+        throw std::runtime_error("Failed to initialize Irrlicht");
     }
 
-    void resizeGL(int w, int h) override {
-        glViewport(0, 0, w, h);
-        glMatrixMode(GL_PROJECTION);
-        glLoadIdentity();
-        gluPerspective(45.0, static_cast<double>(w) / h, 0.1, 100.0);
-        glMatrixMode(GL_MODELVIEW);
+    mDriver = mDevice->getVideoDriver();
+    mSceneMgr = mDevice->getSceneManager();
+    mCamera = mSceneMgr->addCameraSceneNode();
+    mCamera->setPosition(core::vector3df(0, 30, -40));
+   // mCamera->lookAt(core::vector3df(0, 0, 0));
+
+    createScene();
+}
+
+VoxelEditor::~VoxelEditor() {
+    if (mDevice) {
+        mDevice->drop();
     }
+}
 
-    void paintGL() override {
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        glLoadIdentity();
-        gluLookAt(cameraX, cameraY, zoomLevel, cameraX, cameraY, 0.0, 0.0, 1.0, 0.0);
+void VoxelEditor::run() {
+    mRunning = true;
+    while (mDevice->run() && mRunning) {
+        mDriver->beginScene(true, true, video::SColor(255, 100, 101, 140));
+        mSceneMgr->drawAll();
+        mDriver->endScene();
+    }
+}
 
-        for (int x = 0; x < gridSize; ++x) {
-            for (int y = 0; y < gridSize; ++y) {
-                for (int z = 0; z < gridSize; ++z) {
-                    if (voxelGrid[index(x, y, z)]) {
-                        drawVoxel(x, y, z);
+void VoxelEditor::createScene() {
+    // Initialize the scene (currently empty as we dynamically place voxels)
+}
+
+bool VoxelEditor::OnEvent(const SEvent &event) {
+    if (event.EventType == EET_MOUSE_INPUT_EVENT) {
+        switch (event.MouseInput.Event) {
+        case EMIE_LMOUSE_PRESSED_DOWN:
+            mLeftMousePressed = true;
+            break;
+        case EMIE_LMOUSE_LEFT_UP:
+            mLeftMousePressed = false;
+            break;
+        case EMIE_RMOUSE_PRESSED_DOWN:
+            mRightMousePressed = true;
+            break;
+        case EMIE_RMOUSE_LEFT_UP:
+            mRightMousePressed = false;
+            break;
+        case EMIE_MOUSE_MOVED:
+            if (mLeftMousePressed || mRightMousePressed) {
+                core::position2di cursorPos = mDevice->getCursorControl()->getPosition();
+                core::line3df ray = cm->getRayFromScreenCoordinates(cursorPos, mCamera);
+                core::vector3df intersection;
+                core::triangle3df hitTriangle;
+                scene::ISceneNode *selectedNode = cm->getSceneNodeAndCollisionPointFromRay(ray, intersection, hitTriangle);
+
+                if (selectedNode) {
+                    if (mRightMousePressed) {
+                        // Remove voxel on right-click
+                        removeVoxel(selectedNode);
+                    } else if (mLeftMousePressed) {
+                        // Place voxel on left-click
+                        placeVoxel(intersection);
                     }
                 }
             }
+            break;
+        default:
+            break;
         }
     }
+    return false;
+}
 
-    void mousePressEvent(QMouseEvent *event) override {
-        GLint viewport[4];
-        GLdouble modelview[16];
-        GLdouble projection[16];
-        GLfloat winX, winY, winZ;
-        GLdouble posX, posY, posZ;
+void VoxelEditor::placeVoxel(const core::vector3df &position) {
+    scene::IMeshSceneNode *voxel = mSceneMgr->addCubeSceneNode(1.0f, 0, -1, position);
+    voxel->setMaterialFlag(video::EMF_LIGHTING, false);
+    voxel->setMaterialTexture(0, mDriver->getTexture(mCurrentTexture.c_str()));
+    VoxelNode* voxelNode = new VoxelNode(voxel, 0, 1.0f);
+    mVoxelMap[voxel] = voxelNode;
+}
 
-        glGetIntegerv(GL_VIEWPORT, viewport);
-        glGetDoublev(GL_MODELVIEW_MATRIX, modelview);
-        glGetDoublev(GL_PROJECTION_MATRIX, projection);
+void VoxelEditor::removeVoxel(scene::ISceneNode *node) {
+    auto it = mVoxelMap.find(node);
+    if (it != mVoxelMap.end()) {
+        node->remove();
+        delete it->second; // Ensure to delete the VoxelNode pointer
+        mVoxelMap.erase(it);
+    }
+}
 
-        winX = (float)event->x();
-        winY = (float)viewport[3] - (float)event->y() - 1;
-        glReadPixels(event->x(), int(winY), 1, 1, GL_DEPTH_COMPONENT, GL_FLOAT, &winZ);
+void VoxelEditor::scaleVoxel(VoxelNode* voxelNode, float scale) {
+    if (voxelNode) {
+        voxelNode->getNode()->setScale(core::vector3df(scale, scale, scale));
+        voxelNode->setLastSize(scale);
+    }
+}
 
-        gluUnProject(winX, winY, winZ, modelview, projection, viewport, &posX, &posY, &posZ);
+void VoxelEditor::onSelectTexture() {
+    QString filePath = QFileDialog::getOpenFileName(this, tr("Select Texture"), "", tr("Images (*.png *.jpg *.bmp)"));
+    if (!filePath.isEmpty()) {
+        mCurrentTexture = filePath.toStdString();
+    }
+}
 
-        int voxelX = static_cast<int>(qRound(posX / voxelSize + gridSize / 2));
-        int voxelY = static_cast<int>(qRound(posY / voxelSize + gridSize / 2));
-        int voxelZ = static_cast<int>(qRound(posZ / voxelSize + gridSize / 2));
+void VoxelEditor::onUpdate() {
+    if (mDevice) {
+        mDevice->run();
+        mDriver->beginScene(true, true, video::SColor(255, 100, 101, 140));
+        mSceneMgr->drawAll();
+        mDriver->endScene();
+    }
+}
 
-        if (snapToGrid) {
-            voxelX = voxelX - (voxelX % 1);
-            voxelY = voxelY - (voxelY % 1);
-            voxelZ = voxelZ - (voxelZ % 1);
+void VoxelEditor::mousePressEvent(QMouseEvent* event) {
+    if (event->button() == Qt::LeftButton) {
+        mLeftMousePressed = true;
+    } else if (event->button() == Qt::RightButton) {
+        mRightMousePressed = true;
+    }
+}
+
+void VoxelEditor::mouseReleaseEvent(QMouseEvent* event) {
+    if (event->button() == Qt::LeftButton) {
+        mLeftMousePressed = false;
+    } else if (event->button() == Qt::RightButton) {
+        mRightMousePressed = false;
+    }
+}
+
+void VoxelEditor::mouseMoveEvent(QMouseEvent* event) {
+    core::position2di cursorPos(event->pos().x(), event->pos().y());
+    core::line3df ray = cm->getRayFromScreenCoordinates(cursorPos, mCamera);
+    core::vector3df intersection;
+    core::triangle3df hitTriangle;
+    scene::ISceneNode *selectedNode = cm->getSceneNodeAndCollisionPointFromRay(ray, intersection, hitTriangle);
+
+    if (selectedNode) {
+        if (mRightMousePressed) {
+            removeVoxel(selectedNode);
+        } else if (mLeftMousePressed) {
+            placeVoxel(intersection);
         }
-
-        if (voxelX >= 0 && voxelX < gridSize &&
-            voxelY >= 0 && voxelY < gridSize &&
-            voxelZ >= 0 && voxelZ < gridSize) {
-            if (event->button() == Qt::LeftButton) {
-                voxelGrid[index(voxelX, voxelY, voxelZ)] = true;
-            } else if (event->button() == Qt::RightButton) {
-                voxelGrid[index(voxelX, voxelY, voxelZ)] = false;
-            }
-            update();
-        }
     }
-
-    void wheelEvent(QWheelEvent *event) override {
-        zoomLevel -= event->delta() / 120.0f;
-        if (zoomLevel < 1.0f) zoomLevel = 1.0f;
-        update();
-    }
-
-    void mouseMoveEvent(QMouseEvent *event) override {
-        if (event->buttons() & Qt::MiddleButton) {
-            cameraX -= (event->x() - lastMousePosition.x()) / 100.0f;
-            cameraY += (event->y() - lastMousePosition.y()) / 100.0f;
-            update();
-        }
-        lastMousePosition = event->pos();
-    }
-
-private:
-    void drawVoxel(int x, int y, int z) {
-        glPushMatrix();
-        glTranslatef(x * voxelSize - gridSize / 2 * voxelSize, y * voxelSize - gridSize / 2 * voxelSize, z * voxelSize - gridSize / 2 * voxelSize);
-
-        glBegin(GL_QUADS);
-        glColor3f(1.0, 0.0, 0.0);
-
-        // Front face
-        glVertex3f(-0.5, -0.5,  0.5);
-        glVertex3f( 0.5, -0.5,  0.5);
-        glVertex3f( 0.5,  0.5,  0.5);
-        glVertex3f(-0.5,  0.5,  0.5);
-
-        // Back face
-        glVertex3f(-0.5, -0.5, -0.5);
-        glVertex3f( 0.5, -0.5, -0.5);
-        glVertex3f( 0.5,  0.5, -0.5);
-        glVertex3f(-0.5,  0.5, -0.5);
-
-        // Left face
-        glVertex3f(-0.5, -0.5, -0.5);
-        glVertex3f(-0.5, -0.5,  0.5);
-        glVertex3f(-0.5,  0.5,  0.5);
-        glVertex3f(-0.5,  0.5, -0.5);
-
-        // Right face
-        glVertex3f(0.5, -0.5, -0.5);
-        glVertex3f(0.5, -0.5,  0.5);
-        glVertex3f(0.5,  0.5,  0.5);
-        glVertex3f(0.5,  0.5, -0.5);
-
-        // Top face
-        glVertex3f(-0.5,  0.5, -0.5);
-        glVertex3f( 0.5,  0.5, -0.5);
-        glVertex3f( 0.5,  0.5,  0.5);
-        glVertex3f(-0.5,  0.5,  0.5);
-
-        // Bottom face
-        glVertex3f(-0.5, -0.5, -0.5);
-        glVertex3f( 0.5, -0.5, -0.5);
-        glVertex3f( 0.5, -0.5,  0.5);
-        glVertex3f(-0.5, -0.5,  0.5);
-
-        glEnd();
-        glPopMatrix();
-    }
-
-    int index(int x, int y, int z) const {
-        return x + gridSize * (y + gridSize * z);
-    }
-
-    int gridSize;
-    float voxelSize;
-    bool snapToGrid;
-    float zoomLevel;
-    float cameraX, cameraY;
-    QPoint lastMousePosition;
-    std::vector<bool> voxelGrid;
-};
-
-class MainWindow : public QMainWindow {
-    Q_OBJECT
-
-public:
-    MainWindow(QWidget *parent = nullptr)
-        : QMainWindow(parent) {
-        openGLWidget = new OpenGLWidget(this);
-        setCentralWidget(openGLWidget);
-        resize(800, 600); // Set initial window size
-
-        QMenuBar *menuBar = new QMenuBar(this);
-        setMenuBar(menuBar);
-
-        QMenu *fileMenu = menuBar->addMenu("File");
-        QAction *loadAction = fileMenu->addAction("Load");
-        QAction *saveAction = fileMenu->addAction("Save");
-
-    //    connect(loadAction, &QAction::triggered, openGLWidget, &OpenGLWidget::loadVoxels);
-     //   connect(saveAction, &QAction::triggered, openGLWidget, &OpenGLWidget::saveVoxels);
-    }
-
-private:
-    OpenGLWidget *openGLWidget;
-};
+}
 
 int main(int argc, char *argv[]) {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+    QApplication app(argc, argv);
+
+    VoxelEditor editor;
+    editor.show();
+
+    return app.exec();
 }
 
 #include "main.moc"
+
